5691a3ef9f1b3fc9be32c04301571cadab99f4c3
Jose Reginaldo <profjosereginaldo@gmail.com>
2024-10-17 22:01:49 -0300

    codigos da aula9

--- aulas/aula08/src/contexts/TarefaContext.jsx
+++ aulas/aula08/src/contexts/TarefaContext.jsx
@@ -3,17 +3,34 @@ import { createContext, useState } from "react";
 const TarefaContext = createContext();
 
 function TarefaProvider(props) {
-  const [tarefas, setTarefas] = useState(["Estudar react", "Fazer a pratica"]);
+  const [tarefas, setTarefas] = useState([]);
+
+  const carregar = () => {
+    fetch("http://localhost:3000/tarefas")
+      .then((response) => response.json())
+      .then((data) => setTarefas(data))
+      .catch((error) => console.log("Deu ruim"));
+  };
 
   const incluir = (tarefa) => {
-    setTarefas([...tarefas, tarefa]);
-  }
+    fetch("http://localhost:3000/tarefas", {
+      method: "POST",
+      body: JSON.stringify({ nome: tarefa }),
+    })
+      .then((response) => response.json())
+      .then((data) => setTarefas([...tarefas, data]))
+      .catch((error) => console.log("deu ruim"));
+  };
 
   const remover = (tarefa) => {
-    setTarefas(tarefas.filter((item) => item != tarefa));
-  }
+    fetch(`http://localhost:3000/tarefas/${tarefa.id}`, {
+      method: "DELETE",
+    })
+      .then(() => setTarefas(tarefas.filter((item) => item != tarefa)))
+      .catch((error) => console.log("deu ruim"));
+  };
 
-  const contexto = { tarefas, incluir, remover }
+  const contexto = { tarefas, incluir, remover, carregar };
 
   return (
     <TarefaContext.Provider value={contexto}>
